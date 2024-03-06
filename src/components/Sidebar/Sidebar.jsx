957cdc38ad4c0b18ff852d541648a4dea20ac1fe
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-03-06 15:45:53 +0200

    created visual part of component sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,38 @@
-
+import styles from "../../styles/Sidebar.module.scss";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <aside className={styles.sidebar}>
+      <h3 className={styles.title}>Categories</h3>
+      <ul className={styles.list}>
+        <li>
+          <Link to="/">Tehnica</Link>
+        </li>
+        <li>
+          <Link to="/">wefee ee</Link>
+        </li>
+        <li>
+          <Link to="/">Tehniewfwe ca</Link>
+        </li>
+        <li>
+          <Link to="/">wefwefwefw</Link>
+        </li>
+        <li>
+          <Link to="/">werer</Link>
+        </li>
+        <li>
+          <Link to="/">eweeew</Link>
+        </li>
+      </ul>
+      <div className={styles.footer}>
+        <Link to="/">Help</Link>
+        <Link className={styles.conditions} to="/">
+          Terms & Conditions
+        </Link>
+      </div>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
