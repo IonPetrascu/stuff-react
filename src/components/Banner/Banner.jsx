22f5d5c04e95b980c5de62f062ffedd95508b7d6
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-03-06 16:26:03 +0200

    created visual part of banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,22 @@
-
+import Computer from "../../images/computer.png";
+import styles from "../../styles/Banner.module.scss";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className={styles.banner}>
+      <span className={styles.sale}>Big sale 20%</span>
+      <div className={styles.info}>
+        <span className={styles.slogan}>the bestseller of 2024</span>
+        <h1 className={styles.title}>LENNON R2D2 NVIDIA 5090 TI</h1>
+        <button>Shop Now</button>
+      </div>
+      <img
+        className={styles.image}
+        src={Computer}
+        alt="LENNON R2D2 NVIDIA 5090 TI"
+      />
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
-
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Banner />
+    </>
+  );
+};
 
-export default Home
+export default Home;
