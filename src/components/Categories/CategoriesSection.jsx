99b93050116a770accb6d529b79b9677b326d947
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-03-07 17:00:38 +0200

    add:made it accept real categories from the store

--- src/components/Categories/CategoriesSection.jsx
+++ src/components/Categories/CategoriesSection.jsx
@@ -1,69 +1,38 @@
 import { urlImage } from "../../utils/urlImage";
 import styles from "../../styles/CategoriesSection.module.scss";
-
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 const CategoriesSection = () => {
-  return ( 
+  const { list } = useSelector((state) => state.categories);
+  const listCategpories = list.filter((_, id) => id < 5);
+  return (
     <section className={styles.categoriesSection}>
       <h2 className={styles.title}>Worts seeing</h2>
       <div className={styles.list}>
-        <div className={styles.category}>
-          <div
-            className={styles.image}
-            style={{
-              backgroundImage:urlImage(
-                "[https://i.imgur.com/QkIa5tT.jpeg]"
-              ),
-            }}
-          />
-          <h3 className={styles.categoryTitle}>Glothes</h3>
-        </div>
-        <div className={styles.category}>
-          <div
-            className={styles.image}
-            style={{
-              backgroundImage:urlImage(
-                "[https://i.imgur.com/QkIa5tT.jpeg]"
-              ),
-            }}
-          />
-          <h3 className={styles.categoryTitle}>Glothes</h3>
-        </div>
-        <div className={styles.category}>
-          <div
-            className={styles.image}
-            style={{
-              backgroundImage:urlImage(
-                "[https://i.imgur.com/QkIa5tT.jpeg]"
-              ),
-            }}
-          />
-          <h3 className={styles.categoryTitle}>Glothes</h3>
-        </div>
-        <div className={styles.category}>
-          <div
-            className={styles.image}
-            style={{
-              backgroundImage:urlImage(
-                "[https://i.imgur.com/QkIa5tT.jpeg]"
-              ),
-            }}
-          />
-          <h3 className={styles.categoryTitle}>Glothes</h3>
-        </div>
-        <div className={styles.category}>
-          <div
-            className={styles.image}
-            style={{
-              backgroundImage:urlImage(
-                "[https://i.imgur.com/QkIa5tT.jpeg]"
-              ),
-            }}
-          />
-          <h3 className={styles.categoryTitle}>Glothes</h3>
-        </div>
+        {listCategpories.length ? (
+          listCategpories.map(({ id, name, image }) => {
+            return (
+              <Link
+                to={`/categories/${id}`}
+                key={id}
+                className={styles.category}
+              >
+                <div
+                  className={styles.image}
+                  style={{
+                    backgroundImage: urlImage(image),
+                  }}
+                />
+                <h3 className={styles.categoryTitle}>{name}</h3>
+              </Link>
+            );
+          })
+        ) : (
+          <>Loading</>
+        )}
       </div>
     </section>
   );
-}
+};
 
 export default CategoriesSection;
