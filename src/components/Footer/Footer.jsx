6fa7f9aaa35317569c1b30ddfdd02ca7a8f5a1d3
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-03-07 12:18:52 +0200

    created visual part of component footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,24 @@
+import styles from "../../styles/Footer.module.scss";
+import Logo from "../../images/logo.svg";
+
+const socials = ["instagram", "facebook", "youtube"];
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.footer}>
+      <img className={styles.logo} src={Logo} alt="stuff logo" />
+      <span>Developed by Ion Petrascu</span>
+      <div className={styles.socials}>
+        {socials.map((social, index) => {
+          return (
+            <svg className={styles.icon} key={index}>
+              <use xlinkHref={`../../../public/sprite.svg#${social}`} />
+            </svg>
+          );
+        })}
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
