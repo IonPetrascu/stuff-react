220d390ccaa58e28c09c9376f14d79d46fb79d90
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-03-07 14:27:18 +0200

    add : Add display of all data for the products component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,13 @@ import Banner from "../Banner/Banner";
 import Products from "../Products/Products";
 import Poster from "../Poster/Poster";
 import CategoriesSection from "../Categories/CategoriesSection";
-
+import { useSelector } from "react-redux";
 const Home = () => {
+  const { list } = useSelector((state) => state.products);
   return (
     <>
       <Banner />
-      <Products title={"Trands"} />
+      <Products products={list} quantity={10} title={"Trands"} />
       <CategoriesSection />
       <Poster />
     </>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,31 +1,44 @@
 import styles from "../../styles/Products.module.scss";
 import { Link } from "react-router-dom";
+import { srcImage } from "../../utils/urlImage";
+import { discountPrice } from "../../utils/discountPrice";
+import SkeletonCard from "../Skeleton/Card/SkeletonCard";
+import { randomInRange } from "../../utils/randomInRange";
 
-const Products = ({ title, list, quantity }) => {
+const Products = ({ title, products = [], quantity }) => {
+  const list = products.filter((_, id) => id < quantity);
   return (
     <section className={styles.products}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cards}>
-        {[...Array(5)].map((_, id) => {
-          return (
-            <Link to="/" className={styles.card} key={id}>
-              <img
-                className={styles.image}
-                src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg"
-                alt=""
-              />
-              <h3 className={styles['title-card']}>Title of item</h3>
-              <span className={styles['category-card']}>Category</span>
-              <div className={styles.info}>
-                <div className={styles.price}>
-                  <span className={styles.newPrice}>50$</span>
-                  <span className={styles.oldPrice}>40$</span>
-                </div>
-                <span className={styles.count}>14</span>
-              </div>
-            </Link>
-          );
-        })}
+        {list.length
+          ? list.map(({ id, title, images, category, price }) => {
+              return (
+                <Link to={`/products/${id}`} className={styles.card} key={id}>
+                  <img
+                    className={styles.image}
+                    src={srcImage(images[0])}
+                    alt={title}
+                  />
+                  <h3 className={styles["title-card"]}>{title}</h3>
+                  <span className={styles["category-card"]}>
+                    {category.name}
+                  </span>
+                  <div className={styles.info}>
+                    <div className={styles.price}>
+                      <span className={styles.newPrice}>{price}$</span>
+                      <span className={styles.oldPrice}>
+                        {discountPrice(20, price)}$
+                      </span>
+                    </div>
+                    <span className={styles.count}>{randomInRange(2, 25)}</span>
+                  </div>
+                </Link>
+              );
+            })
+          : [...Array(5)].map((_, id) => {
+              return <SkeletonCard key={id} />;
+            })}
       </div>
     </section>
   );
