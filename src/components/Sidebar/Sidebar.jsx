3f38791132b5ed6319a45c42c25db7674f0a46f4
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-03-07 13:08:22 +0200

    Implemented retrieving categories from Redux storage and displaying them in the Sidebar component ,and icon for loading

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,29 +1,27 @@
 import styles from "../../styles/Sidebar.module.scss";
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
+import Loading from "../../images/loading.svg";
 const Sidebar = () => {
+  const { list, isLoading } = useSelector((state) => state.categories);
+
   return (
     <aside className={styles.sidebar}>
       <h3 className={styles.title}>Categories</h3>
       <ul className={styles.list}>
-        <li>
-          <Link to="/">Tehnica</Link>
-        </li>
-        <li>
-          <Link to="/">wefee ee</Link>
-        </li>
-        <li>
-          <Link to="/">Tehniewfwe ca</Link>
-        </li>
-        <li>
-          <Link to="/">wefwefwefw</Link>
-        </li>
-        <li>
-          <Link to="/">werer</Link>
-        </li>
-        <li>
-          <Link to="/">eweeew</Link>
-        </li>
+        {!list.length && isLoading ? (
+          <div>
+            <img className={styles.loading} src={Loading} alt="loading" />
+          </div>
+        ) : (
+          list.map(({ name, id }) => {
+            return (
+              <li key={id}>
+                <Link to={`/categories/${id}`}>{name}</Link>
+              </li>
+            );
+          })
+        )}
       </ul>
       <div className={styles.footer}>
         <Link to="/">Help</Link>
