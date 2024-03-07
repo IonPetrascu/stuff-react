66a63017b0394fc5f95dde4b17c671c7b56716c6
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-03-07 16:55:58 +0200

    fixed:Changed  Route component for Category Page

--- src/components/Routes/AppRoutes.jsx
+++ src/components/Routes/AppRoutes.jsx
@@ -3,14 +3,14 @@ import { ROUTES } from "../../utils/routes";
 
 import Home from "../Home/Home";
 import Cart from "../Cart/Cart";
-import CategoriesSection from "../Categories/CategoriesSection";
+import CategoryPage from "../Categories/CategoryPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
       <Route path={ROUTES.CART} element={<Cart />} />
-      <Route path={ROUTES.CATEGORY} element={<CategoriesSection />} />
+      <Route path={ROUTES.CATEGORY} element={<CategoryPage />} />
     </Routes>
   );
 };
